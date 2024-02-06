00a9991d10ee993a309a4e4db96d95a1e12da2e0
sandeep-vedam <sandeepsharma.vedam@outlook.com>
2024-02-06 15:29:20 +0530

    Added initial version

---
+++ bin/index.js
@@ -0,0 +1,40 @@
+console.log('sandeep' + 'Kumar')
+import prompts from 'prompts'
+import { red, bold } from 'kolorist'
+import ora from 'ora'
+const spinner = ora()
+
+
+// let x = 10
+// let y = 20
+
+// if(x==y){
+//   console.log("Hello")
+// }
+//  else {
+//    console.log("Hi")
+// }
+
+const questions = [
+  {
+    type: 'text',
+    name: 'q1',
+    message: 'Whats your name',
+    initial: 'Sample',
+  },
+]
+
+
+const createApp = () => {
+  console.log("Inside create app")
+  return new Promise(async resolve => {
+  
+      console.log("Inside promise")
+      let answ
+        answ = await prompts(questions)
+        spinner.succeed(`${answ.q1}`)
+  })
+  }
+
+createApp()
+
